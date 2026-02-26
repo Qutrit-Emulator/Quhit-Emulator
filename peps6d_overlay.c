69988c11fc61c3de9428940b0b7ee77e8b770b32
HexState <Waves3322@proton.me>
2026-02-26 02:21:00 -0500

    Replace tsvd_truncated with tsvd_truncated_sparse

--- peps6d_overlay.c
+++ peps6d_overlay.c
@@ -258,7 +258,7 @@ static void tns6d_gate_2site_generic(Tns6dGrid *g, int sA, int sB,
     double *Vr=(double*)calloc((size_t)chi*sdB,sizeof(double));
     double *Vi=(double*)calloc((size_t)chi*sdB,sizeof(double));
 
-    tsvd_truncated(T2r,T2i,sdA,sdB,chi,Ur,Ui,sig,Vr,Vi);
+    tsvd_truncated_sparse(T2r,T2i,sdA,sdB,chi,Ur,Ui,sig,Vr,Vi);
     free(T2r);free(T2i);
 
     int rank=chi<sdB?chi:sdB; if(rank>sdA) rank=sdA;
